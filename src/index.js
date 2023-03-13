bafc72d8805ccf273824bc1573e2dfd88f58d06c
katk4tkat <katk4tkat@gmail.com>
2023-03-13 12:03:45 -0300

    esquleto terminado

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 import validator from './validator.js';
 
+let User = document.getElementById ("userName")
+
+User.addEventListener("keyup",()=> console.log(User.value))
+
+let TC = document.getElementById ("creditCard")
+
+TC.addEventListener("keyup",function() {
+    console.log(TC.value.split(""))
+})
+
+
 console.log(validator);
